7abbb9d24b2ee85f555db2eefae5abb41edfe009
Gileard T Santos <111284002+Gil-Ti@users.noreply.github.com>
2023-01-31 19:23:45 -0300

    Criação do Botão de Upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="conversor.css">
+</head>
+    <body>
+        <label for="file-upload" class="custom-file-upload">
+        Clique aqui para selecionar um arquivo
+        </label>
+        <input type="file" id="file-upload" />
+    </body>
+</html>
