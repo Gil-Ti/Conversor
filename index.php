75b3cd8c90ef888cd6b3359d96f87e34380e8dd6
Gileard T Santos <111284002+Gil-Ti@users.noreply.github.com>
2023-01-31 19:28:21 -0300

    Revisão do texto

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conversor PDF -> XLSX</title>
 
     <link rel="stylesheet" href="reset.css">
 	<link rel="stylesheet" href="conversor.css">
 </head>
     <body>
         <label for="file-upload" class="custom-file-upload">
-        Clique aqui para selecionar um arquivo
+        Clique aqui para selecionar o seu PDF
         </label>
         <input type="file" id="file-upload" />
     </body>
