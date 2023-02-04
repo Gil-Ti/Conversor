daa36164b3eb07cb931479cf10ec695074f47d10
Gileard T Santos <111284002+Gil-Ti@users.noreply.github.com>
2023-02-03 22:54:38 -0300

    Direcionando o caminho da pasta

--- convertepdf.py
+++ convertepdf.py
@@ -2,9 +2,9 @@ import tabula
 import pandas as pd
 
 # Lê o PDF usando a biblioteca tabula
-dfs = tabula.read_pdf("ResultadoVale.pdf", pages="all")
+dfs = tabula.read_pdf("PDF/ResultadoVale.pdf", pages="all")
 
 # Loop para cada dataframe na lista
 for i, df in enumerate(dfs):
     # Salva o dataframe em um arquivo Excel
-    df.to_excel("tabela_extraída_" + str(i) + ".xlsx", index=False)
+    df.to_excel("Excel/tabela_extraída_" + str(i) + ".xlsx", index=False)
